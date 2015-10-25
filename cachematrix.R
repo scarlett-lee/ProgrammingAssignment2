46742f2ada66d6a5a9e5891127fd3e393a42e653
coolpenguin37 <coolpenguin37@gmail.com>
2015-10-26 00:57:37 +0800

    Final Version

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,11 @@
 
 ## Write a short comment describing this function
 
+##@makeCacheMatrix: creates a special "matrix", which is really a list containing a function to 
+##@set:set the value of the matrix
+##@get:get the value of the matrix
+##@setsolve: set the solve of the matrix
+##@getsolve: get the solve of the matrix
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -10,7 +15,7 @@ makeCacheMatrix <- function(x = matrix()) {
         m <<- NULL
     }
     get <- function() x
-    setsolve <- function(solve) m <<- mean
+    setsolve <- function(solve) m <<- solve
     getsolve <- function() m
     list(set = set, get = get,
         setsolve = setsolve,
@@ -19,7 +24,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## @cacheSolve: calculates the solve of the special "matrix" created with the above function
+## If the solve is computed before, then it will get the solve from the cache
+## If the solve is NOT computed before, then it will compute and cache it
 cacheSolve <- function(x, ...) {
     m <- x$getsolve()
     if(!is.null(m)) {
@@ -33,15 +40,16 @@ cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
 }
 
-t <- matrix(c(1,1,1,1), nrow = 2, ncol = 2)
-print(t)
-bubba <- makeCacheMatrix()
-bubba$set(t)
-bubba$get()
-cacheSolve(bubba)
 
-# zzz <- makeCacheMatrix$set(matrix(c(2, 1, 1, -5, -3, 0, 1, 1, -1), nrow = 3, ncol = 3))
-
-# print(zzz.get())
-
-# cacheSolve(tmp)
+# Example
+# t <- matrix(c(3,1,0,2,1,1,1,-1,2), nrow = 3, ncol = 3)
+# p <- matrix(c(6,-2,-7,4), nrow = 2, ncol = 2)
+# bubba <- makeCacheMatrix()
+# bubba$set(t)
+# bubba$get()
+# cacheSolve(bubba)
+# cacheSolve(bubba)
+# bubba$set(p)
+# bubba$get()
+# cacheSolve(bubba)
+# cacheSolve(bubba)
